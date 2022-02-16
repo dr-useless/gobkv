aa7d3e9f8324f2350ced85d24d031926e56d8193
Joseph Innes <joeinnes0@gmail.com>
2022-02-16 12:58:36 +0100

    Update pprof.go

--- pprof.go
+++ pprof.go
@@ -41,8 +41,8 @@ func makeMemProfile() {
 	if err != nil {
 		log.Fatal("could not create memory profile: ", err)
 	}
-	defer f.Close() // error handling omitted for example
-	runtime.GC()    // get up-to-date statistics
+	defer f.Close()
+	runtime.GC() // get up-to-date statistics
 	if err := pprof.WriteHeapProfile(f); err != nil {
 		log.Fatal("could not write memory profile: ", err)
 	}
